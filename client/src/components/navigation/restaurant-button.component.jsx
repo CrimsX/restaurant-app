d9d95c76e061add25227c6edeec1bdb81ea5f550
klearliu <clearlynetworking@gmail.com>
2024-03-17 17:48:48 -0600

    front end setup

--- client/src/components/navigation/restaurant-button.component.jsx
+++ client/src/components/navigation/restaurant-button.component.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./restaurant-button.styles.css"
-import "bootstrap/dist/css/bootstrap.min.css"
+import "./restaurant-button.styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const NavToRestaurantApp = () => {
-    let navigate = useNavigate();
-    const routeToRestaurantApp = () => {
-        navigate(`/restaurant-client`)
-    }
+  let navigate = useNavigate();
+  const routeToRestaurantApp = () => {
+    navigate(`/RestaurantInterface/home`);
+  };
 
-    return (
-        <button type="button" class="btn btn-success btn-custom"
-            onClick={routeToRestaurantApp}>
-                Restaurant Interface
-        </button>
-    )
+  return (
+    <button
+      type="button"
+      class="btn btn-success btn-custom"
+      onClick={routeToRestaurantApp}
+    >
+      Restaurant Interface
+    </button>
+  );
 };
---
+++ client/src/interfaces/restaurant-interface/ManagerHome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ManagerHome = () => {
+  return <div>Home Page Of Restaurant Management</div>;
+};
+
+export default ManagerHome;
