a8f04e0470f980365f9a455741d0a7b387000337
klearliu <clearlynetworking@gmail.com>
2024-03-18 13:12:10 -0600

    Improved UI: table cells are now at a fixed size

--- client/src/interfaces/restaurant-interface/ManagerHome.jsx
+++ client/src/interfaces/restaurant-interface/ManagerHome.jsx
@@ -17,8 +17,11 @@ const ManagerHome = () => {
     setLoading(true);
     const fetchData = async () => {
       try {
+        // Gathering menu data
         const menuData1 = await getMenu(1);
         const menuData2 = await getMenu(2);
+
+        // Combining arrays ... is a spread operator
         const combinedItems = [...menuData1.data, ...menuData2.data];
         setItems(combinedItems);
         setLoading(false);
@@ -36,13 +39,13 @@ const ManagerHome = () => {
 
   return (
     <div className="p-4">
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl my-8">All Items</h1>
-        <div>
-          <label>Choose an option: </label>
-          <br></br>
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-3xl">All Items</h1>
+        <div className="flex items-center">
+          <label className="mr-2">Choose an option:</label>
           <select
             id="dropdown"
+            className="border rounded px-2 py-1"
             onChange={handleDropdownChange}
             value={selectedRestaurant}
           >
@@ -51,23 +54,22 @@ const ManagerHome = () => {
             <option value="Asianres">Asianres</option>
           </select>
         </div>
-
         <Link to="/items/create">
-          <MdOutlineAddBox className="text-sky-800 text-4xl" />
+          <MdOutlineAddBox className="text-sky-800 text-4xl ml-4" />
         </Link>
       </div>
       {loading ? (
         <Spinner />
       ) : (
-        <table className="w-full border-separate border-spacing-2">
-          <thead>
+        <table className="w-full border-collapse border border-gray-300">
+          <thead className="bg-gray-400">
             <tr className="text-center">
-              <th>Item ID</th>
-              <th>Food Item</th>
-              <th>Availability</th>
-              <th>Restaurant Branch</th>
-              <th>Price</th>
-              <th>Actions</th>
+              <th className="py-2 w-1/6">Item ID</th>
+              <th className="py-2 w-2/6">Food Item</th>
+              <th className="py-2 w-1/6">Availability</th>
+              <th className="py-2 w-1/6">Restaurant Branch</th>
+              <th className="py-2 w-1/6">Price</th>
+              <th className="py-2 w-1/6">Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -79,24 +81,29 @@ const ManagerHome = () => {
                   (selectedRestaurant === "Asianres" && item.rid === 2)
               )
               .map((item, index) => (
-                <tr key={item._id} className="h-8">
-                  <td className="border text-center">{index + 1}</td>
-                  <td className="border text-center">{item.name}</td>
-                  <td className="border text-center">Available</td>
-                  <td className="border text-center">
+                <tr
+                  key={item._id}
+                  className={index % 2 === 0 ? "bg-gray-100" : ""}
+                >
+                  <td className="py-2 border text-center"> {index + 1} </td>
+                  <td className="py-2 border text-center"> {item.name} </td>
+                  <td className="py-2 border text-center"> Available </td>
+                  <td className="py-2 border text-center">
                     {item.rid === 1 ? "Deliscio" : "Asianres"}
                   </td>
-                  <td className="border text-center">${item.price / 100}</td>
-                  <td className="border text-center">
-                    <div className="flex justify-center gap-x-4">
+                  <td className="py-2 border text-center">
+                    ${item.price / 100}
+                  </td>
+                  <td className="py-2 px-5 border text-center">
+                    <div className="flex justify-center gap-x-5">
                       <Link to={`/items/details/${item._id}`}>
-                        <BsInfoCircle className="text-2xl text-green-800" />
+                        <BsInfoCircle className="text-2xl text-green-800 hover:text-green-600" />
                       </Link>
                       <Link to={`/items/edit/${item._id}`}>
-                        <AiOutlineEdit className="text-2xl text-yellow-600" />
+                        <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-yellow-400" />
                       </Link>
                       <Link to={`/items/delete/${item._id}`}>
-                        <MdOutlineDelete className="text-2xl text-red-600" />
+                        <MdOutlineDelete className="text-2xl text-red-600 hover:text-red-400" />
                       </Link>
                     </div>
                   </td>
