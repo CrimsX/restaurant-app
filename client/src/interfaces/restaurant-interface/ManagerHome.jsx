6a8fb6e202fa0c3ad09400ac28de7761fa8d1117
klearliu <clearlynetworking@gmail.com>
2024-03-19 13:12:07 -0600

    added mouse hovering effect on table

--- client/src/interfaces/restaurant-interface/ManagerHome.jsx
+++ client/src/interfaces/restaurant-interface/ManagerHome.jsx
@@ -40,9 +40,9 @@ const ManagerHome = () => {
   return (
     <div className="p-4">
       <div className="flex justify-between items-center mb-4">
-        <h1 className="text-3xl">All Items</h1>
+        <h1 className="text-3xl">Items</h1>
         <div className="flex items-center">
-          <label className="mr-2">Choose an option:</label>
+          <label className="mr-2">Manager Login:</label>
           <select
             id="dropdown"
             className="border rounded px-2 py-1"
@@ -59,10 +59,19 @@ const ManagerHome = () => {
         </Link>
       </div>
       {loading ? (
-        <Spinner />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <Spinner />
+        </div>
       ) : (
         <table className="w-full border-collapse border border-gray-300">
-          <thead className="bg-gray-400">
+          <thead className="bg-gray-200">
             <tr className="text-center">
               <th className="py-2 w-1/6">Item ID</th>
               <th className="py-2 w-2/6">Food Item</th>
@@ -84,10 +93,20 @@ const ManagerHome = () => {
                 <tr
                   key={item._id}
                   className={index % 2 === 0 ? "bg-gray-100" : ""}
+                  style={{ transition: "background-color 0.25s" }}
+                  // mouse hover effects on table
+                  onMouseEnter={(e) => {
+                    e.target.parentElement.classList.add("hover:bg-green-200");
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.parentElement.classList.remove(
+                      "hover:bg-gray-200"
+                    );
+                  }}
                 >
-                  <td className="py-2 border text-center"> {index + 1} </td>
-                  <td className="py-2 border text-center"> {item.name} </td>
-                  <td className="py-2 border text-center"> Available </td>
+                  <td className="py-2 border text-center">{item._id}</td>
+                  <td className="py-2 border text-center">{item.name}</td>
+                  <td className="py-2 border text-center">Available</td>
                   <td className="py-2 border text-center">
                     {item.rid === 1 ? "Deliscio" : "Asianres"}
                   </td>
@@ -97,10 +116,10 @@ const ManagerHome = () => {
                   <td className="py-2 px-5 border text-center">
                     <div className="flex justify-center gap-x-5">
                       <Link to={`/items/details/${item._id}`}>
-                        <BsInfoCircle className="text-2xl text-green-800 hover:text-green-600" />
+                        <BsInfoCircle className="text-2xl text-blue-600 hover:text-blue-400" />
                       </Link>
                       <Link to={`/items/edit/${item._id}`}>
-                        <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-yellow-400" />
+                        <AiOutlineEdit className="text-2xl text-green-600 hover:text-green-400" />
                       </Link>
                       <Link to={`/items/delete/${item._id}`}>
                         <MdOutlineDelete className="text-2xl text-red-600 hover:text-red-400" />
