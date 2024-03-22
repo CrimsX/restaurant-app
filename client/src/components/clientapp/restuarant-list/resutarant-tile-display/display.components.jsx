a53bbb0b5b40c21988be89bbd697a58d3ff5a831
Jordan Lai <jlai.github@gmail.com>
2024-03-21 19:45:37 -0600

    fetch and display restaurants from db

--- client/src/components/clientapp/restuarant-list/resutarant-tile-display/display.components.jsx
+++ client/src/components/clientapp/restuarant-list/resutarant-tile-display/display.components.jsx
@@ -3,9 +3,9 @@ import {Tile} from '../restaurant-tile/tile.components';
 import "./display.styles.css";
 
 //Component to organize tiles into a grid. Called in landingScreen.JS as "Restaurants"
-export const TileList = ({ restuarants }) => (
+export const TileList = ({ restaurants }) => (
     <div className="tilelist">
-        {restuarants.map(restaurant => (
+        {restaurants.map(restaurant => (
         <Tile key={restaurant.id} restaurant={restaurant} />
         ))}
     </div>
--- client/src/interfaces/client-interface/landing/landingScreen.js
+++ client/src/interfaces/client-interface/landing/landingScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState }from 'react';
+import axios from 'axios';
 import { NavBar } from'../../../components/clientapp/navbar/navbar.components'
 import './landingScreen.css'
 import Restaurants from '../../../components/clientapp/restuarant-list/resutarant-tile-display/display.components'
@@ -8,33 +9,11 @@ function Home(data) {
     const [restaurants, setRestaurants] = useState([]);
     const [cartItems, setCartItems] = useState([]);
 
-    /*
-    TODO: fetch list of restuarants from db in the useEffect
-    */
     useEffect( () => {
-        const test = [
-            {
-              "name": "McDonald's",
-              "rid": 12345
-            },
-            {
-              "name": "Pizza Hut",
-              "rid": 67890
-            },
-            {
-              "name": "Subway",
-              "rid": 23456
-            },
-            {
-              "name": "Starbucks",
-              "rid": 78901
-            },
-            {
-              "name": "Burger King",
-              "rid": 34567
-            }
-          ]
-        setRestaurants(test);
+      axios.get('http://localhost:8000/restaurant/restaurants')
+      .then((res) => {
+        setRestaurants(res.data.data);
+      })
     }, []);
 
     return (
@@ -42,7 +21,7 @@ function Home(data) {
             <NavBar cartItems={cartItems}/>
             <div className='body'>
               <h1 className='title'>Restaurant's</h1>
-              <Restaurants restuarants={restaurants} onClick/>
+              <Restaurants restaurants={restaurants} onClick/>
             </div>
         </div>
 
