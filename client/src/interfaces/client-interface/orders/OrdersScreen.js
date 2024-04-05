6d69a4f9b246457ae860e92fa4dd4cbc7365c402
Jordan Lai <jlai.github@gmail.com>
2024-04-05 01:36:22 -0600

    removed old implementation

--- client/src/interfaces/client-interface/orders/OrdersScreen.js
+++ client/src/interfaces/client-interface/orders/OrdersScreen.js
@@ -1,27 +1,22 @@
+import React, { useEffect, useState }from 'react';
+
+import { useParams } from 'react-router-dom';
+import { useQuery } from "@tanstack/react-query"; //react-query
+import { getAllOrdersP, getOrdersHistory } from '../../../actions/customerAction';
+
+import './OrdersScreen.css';
+
+import { NavBar } from'../../../components/clientapp/navbar/navbar.components'
+import Accordion from 'react-bootstrap/Accordion';
+import OrdersInfo from './OrderInfoTable.component';
+
 /*
 display menu item as a grid
     when clicked, ask for quantity to add
         on the same pop up, click add to cart or cancel
 
 */
-import React, { useEffect, useState }from 'react';
-import { NavBar } from'../../../components/clientapp/navbar/navbar.components'
 
-import { useParams } from 'react-router-dom';
-
-import Button from 'react-bootstrap/Button';
-
-import { getAllOrdersP, getOrdersHistory } from '../../../actions/customerAction';
-
-import './OrdersScreen.css';
-//import axios from 'axios';
-import Accordion from 'react-bootstrap/Accordion';
-
-import { useQuery } from "@tanstack/react-query"; //react-query
-//import { getOrdersHistory } from '../../../actions/restaurantAction';
-import OrdersInfo from './OrderInfoTable.component';
-//import { getPrice } from '../../restaurant-interface/DisplayItemFunctions';
-//
 /*
 buttons on nav bar (sticky buttons for now):
         view cart (enddrawer from the right (component))
@@ -31,32 +26,35 @@ buttons on nav bar (sticky buttons for now):
 List restaurants
 */
 
-
 /*
  * cart/reorder/2
  * reorder
   * rid
   * order_id
 */
-function Order() {
-    const [cartItems, setCartItems] = useState([])
-    const {cid} = useParams();
-    const [orders, setOrders] = useState([])
-    const [restaurants, setRestaurants] = useState([])
-    const [expandedOrderID, setExpandedOrderID] = useState(null);
-    let i = 0;
 
-const {isError, isSuccess, isLoading, data} = useQuery({
+function Order() {
+  const [cartItems, setCartItems] = useState([])
+  const {cid} = useParams();
+  const [orders, setOrders] = useState([])
+  const [restaurants, setRestaurants] = useState([])
+  const [expandedOrderID, setExpandedOrderID] = useState(null);
+  let i = 0;
+
+  // Get order history
+  const {isError, isSuccess, isLoading, data} = useQuery({
     queryKey: ["history", cid],
     queryFn:() => getOrdersHistory(cid),
   });
 
+  // Error message
   if (isError) {
     return (
       <h4>Unable to retrieve list of orders </h4>
     )
   }
 
+  // Loading message
   if (isLoading) {
     return (
       <h4>Please wait, retrieving orders history </h4>
@@ -70,51 +68,25 @@ const {isError, isSuccess, isLoading, data} = useQuery({
 
   // Convert date to locale date
   const convertDate = (date) => {
-  let locale = new Date(date);
-  return locale.toLocaleDateString();
-}
+    let locale = new Date(date);
+    return locale.toLocaleDateString();
+  }
 
-// Convert order status to readable string
-const convertOrderStatus = (status) => {
-  switch(status) {
-    case 1:
+  // Convert order status to readable string
+  const convertOrderStatus = (status) => {
+    switch(status) {
+      case 1:
         return "In-progress";
-    case 2: 
+      case 2: 
         return "Awaiting-Pickup"
-    case 3: 
+      case 3: 
         return "Completed"
-    default:
+      default:
         return "Ordered"
-  }
-}
-
-  /*
-  useEffect(() => {
-  const url = 'http://localhost:8000/customer/orders/all/1';
-    axios.get(url)
-    .then((res) => {
-      //console.log(res.data.data);
-        setOrders(res.data.data);
-      })
-      }, []);
-      */
-
-    const displayID = () => {
-      //console.log(cid);
-      //console.log(getAllOrdersP(cid));
-      //console.log(getOrdersHistory(cid));
-      //setOrders(getOrdersHistory(cid));
-      //console.log(getOrdersHistoryMonth(cid));
-      console.log(orders);
-    console.log(restaurants[0].name);
-    console.log(orders[0].items[0].item.name);
-    console.log(orders[0].items[0].name);
     }
-
-  const toggleRow = (orderID) => {
-    setExpandedOrderID(orderID === expandedOrderID ? null : orderID);
   }
 
+  // Change quantity of item in order history
   const changeQuantity = (quantity) => {
     // change quantity
   }
@@ -128,24 +100,24 @@ const convertOrderStatus = (status) => {
           <div className="orderInfoTable">
             <Accordion>
             {
-            data.data.map(order => (
-              <Accordion.Item key={i++} eventKey={i++}>
-                <Accordion.Header>
-                  Restaurant: {order.restaurant.name} &emsp; &emsp; 
-                  Order ID: {order.order_id} &emsp; &emsp; 
-                  Date: {convertDate(order.orderAt)} &emsp; &emsp; 
-                  Order Status: {convertOrderStatus(order.status)}
-                </Accordion.Header>
-                <Accordion.Body>
-                  <p>
-                    Name: {order.customer.name} <br></br> 
-                    Customer ID: {order.customer.cid} <br></br> 
-                  </p>
-                  <OrdersInfo order={order.items}> </OrdersInfo>
-                  <h5>Grand Total: ${getPrice(order.total)}</h5>
-                </Accordion.Body>
-              </Accordion.Item>
-            ))
+              data.data.map(order => (
+                <Accordion.Item key={i++} eventKey={i++}>
+                  <Accordion.Header>
+                    Restaurant: {order.restaurant.name} &emsp; &emsp; 
+                    Order ID: {order.order_id} &emsp; &emsp; 
+                    Date: {convertDate(order.orderAt)} &emsp; &emsp; 
+                    Order Status: {convertOrderStatus(order.status)}
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <p>
+                      Name: {order.customer.name} <br></br> 
+                      Customer ID: {order.customer.cid} <br></br> 
+                    </p>
+                    <OrdersInfo order={order.items}> </OrdersInfo>
+                    <h5>Grand Total: ${getPrice(order.total)}</h5>
+                  </Accordion.Body>
+                </Accordion.Item>
+              ))
             }
             </Accordion>
           </div>
@@ -154,69 +126,5 @@ const convertOrderStatus = (status) => {
     }
   }
 }
-
-/*
-    return (
-        <div>
-        <NavBar cartItems={cartItems}/>
-      <h1>Order History</h1>
-      
-      <table className="ordersTable">
-        <thead>
-        <tr>
-        <th>Restaurant</th>
-        <th>Total Cost</th>
-        <th>View Items</th>
-      </tr>
-      </thead>
-          <tbody>
-          {orders.map(order => (
-            <tr key={order._id}>
-              
-              <td>{order.restaurant.name}</td>
-              <td>{(order.total / 100).toFixed(2)}</td>
-              <td>
-              <button onClick={() => toggleRow(order._id)}>
-                {expandedOrderID === order._id ? 'Hide Items' : 'Show Items'}
-                </button>
-              </td>
-              
-            {expandedOrderID === order._id && (
-            <tr>
-            <td colSpan={orders.length}>
-                    <table>
-                      <thead>
-                    <tr>
-                    <th>Item</th>
-                    <th>Quantity</th>
-                    <th>Cost</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-            {order.items.map(item => (
-            <tr key={item._id}>
-              <td>{item.item.name}</td>
-            <td>{item.quantity}</td>
-            <td>{(item.total / 100).toFixed(2)}</td>
-            <button onClick={() => changeQuantity(item.quantity)}>Change Quantity</button>
-
-
-            </tr>
-                        ))
-            }
-            </tbody>
-            </table>
-            </td>
-            </tr>
-              )}
-            </tr>
-            ))}
-        </tbody>
-      </table>
-      <Button onClick={displayID}>test</Button> 
-      
-      </div>
-    )}
-*/
 
 export default Order;
